a532cdbfb3e75749ab155821d3ffe0a2a60bd933
herfables <115568949+herfables@users.noreply.github.com>
2022-10-11 20:24:07 +0530

    Add files via upload

---
+++ 02_04/script2.js
@@ -0,0 +1,17 @@
+// const backpack = {
+//   name: "Everyday Backpack",
+//   volume: 30,
+//   color: "grey",
+//   pocketNum: 15,
+//   strapLength: {
+//     left: 26,
+//     right: 26,
+//   },
+
+const backpack = {
+  name: "Red backpack",
+  volume: 25,
+  color: "blue",
+  chains: 5,
+};
+console.log(backpack);
